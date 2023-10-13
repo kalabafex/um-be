768cd9497bd9ffb040e2838f0b950ffbee4d5732
kalab Afera <kalabafex@gmail.com>
2023-10-13 06:46:02 -0500

    init

---
+++ src/test/java/com/um/umbe/UmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.um.umbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
